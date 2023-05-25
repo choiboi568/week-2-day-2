d78eeb9a6c77ba36a37cf2e14dd1db4db4090d39
choiboi568 <choi_robin@yahoo.com>
2023-05-24 23:03:09 -0700

    first commit

---
+++ .ipynb_checkpoints/b.module.py
@@ -0,0 +1,9 @@
+import math
+
+def calculate_square_footage(length, width):
+    area = length * width
+    return area
+
+def calculate_circumference(radius):
+    circumference = 2 * math.pi * radius
+    return circumference
